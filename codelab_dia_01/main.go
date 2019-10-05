622d2208b247f1449f50b6e8ce3ff449ba3e78bb
becky <luannagobbato@gmail.com>
2019-10-05 18:06:55 -0300

    ex 1, 2 e 3
    - Adicionadas as resoluções nos arquivos main dos exercícios.
    - Apagado o arquivo ex1 que foi adicionado incorretamente

--- codelab_dia_01/main.go
+++ codelab_dia_01/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	println("coloque suas resoluções nesse arquivo :D")
+	a, b := 230, 27
+	var soma= a + b
+	var sub= a - b
+	fmt.Println("valores:")
+	fmt.Println("a = ", a)
+	fmt.Println("b = ", b)
+	fmt.Println("soma = ", soma, "\nsubtração = ", sub)
 }
--- codelab_dia_02/main.go
+++ codelab_dia_02/main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("coloque suas resoluções nesse arquivo :D")
+	a, b := 230, 27
+	var soma= a + b
+	var sub= a - b
+	var mult= a * b
+	var div = float64(a) / float64(b)
+
+	fmt.Println("valores:")
+	fmt.Println("a = ", a)
+	fmt.Println("b = ", b)
+	fmt.Println("soma = ", soma, "\nsubtração = ", sub, "\nMultiplicação= ", mult, "\ndivisão= ",div )
 }
