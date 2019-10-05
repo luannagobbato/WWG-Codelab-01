299674cb5767d2fb9cce1fc882de194ee77f0958
becky <luannagobbato@gmail.com>
2019-10-05 17:15:18 -0300

    Exercício 1

---
+++ codelab_dia_01/ex1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	a, b := 230, 27
+	var soma= a+b
+	var sub= a-b
+	fmt.Println("valores:")
+	fmt.Println("a =", a)
+	fmt.Println("b =", b)
+	fmt.Println("soma = ", soma, "\nsubtração =", sub)
+}
