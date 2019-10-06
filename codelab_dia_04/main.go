9a84dc162381af6a057057e196af770ce1906762
becky <luannagobbato@gmail.com>
2019-10-06 20:27:36 -0300

    tarefa 4 finalizada

--- codelab_dia_04/main.go
+++ codelab_dia_04/main.go
@@ -1,5 +1,33 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("coloque suas resoluções nesse arquivo :D")
+	//#04
+	//Preços dos itens do mercado
+
+	var preçoDoPão float64 = 4.60
+	var preçoDaAveia float64 = 5
+	var preçoDoAzeite float64 = 19.95
+	var preçoDoSuco float64 = 7
+	var preçoDaÁgua float64 = 2.15
+	var preçoDoKGMaçã float64 = 8.90
+	var preçoDoKGBanana float64 = 4.5
+
+	/*- 3 pães
+		- 1 azeite
+	- 2 garrafas de suco de laranja
+	- 5 garrafas de água
+	- 1.5 kg de maçã
+	*/
+
+	custoCompra := preçoDoPão * 3 +
+		preçoDoAzeite * 1 +
+		preçoDoSuco * 2 +
+		preçoDaÁgua * 5 +
+		preçoDoKGMaçã * 1.5 +
+		preçoDoKGBanana * 0 +
+		preçoDaAveia * 0
+
+	fmt.Println("O preço total da compra foi: R$",custoCompra )
 }
