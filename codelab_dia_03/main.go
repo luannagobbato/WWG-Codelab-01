44ae26d6989e411cce8930e81195feea9541cedb
becky <luannagobbato@gmail.com>
2019-10-06 21:09:54 -0300

    Correção de arquivos
    Corrigindo novamente os arquivos das pastas do codelab, cada uma com os exercícios referentes a cada dia

--- codelab_dia_03/main.go
+++ codelab_dia_03/main.go
@@ -1,19 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	a, b := 230, 27
-	var soma = a + b
-	var sub = a - b
-	var mult = a * b
-	var div = float64(a) / float64(b)
 
-	fmt.Println("valores:")
-	fmt.Println("a =", a, "\nb =", b)
-	fmt.Println("soma = ", soma, "\nsubtração =", sub)
-	fmt.Println("multiplicação =", mult, "\ndivisão =", div)
-
-	// #3
-	fmt.Printf("\nTipos\na: %T\nb: %T\nSoma: %T\n Subtração: %T\nDivisão: %T\nMultiplicação: %T", a,b,soma,sub,div,mult)
 }
